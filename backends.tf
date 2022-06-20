a2bbc3fd44a8e3b5a694665da35cf136969bbe6f
Quang Tong <101450978+QuangTVTD@users.noreply.github.com>
2022-06-20 20:31:40 +0700

    Update backends.tf

--- backends.tf
+++ backends.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-s3"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-southeast-1"
     dynamodb_table = "terraform-state-lock"
 
   }
