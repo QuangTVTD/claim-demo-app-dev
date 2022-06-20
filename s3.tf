108bf82207e7ee369a0b4da6d677881ab99da203
Quang Tong <101450978+QuangTVTD@users.noreply.github.com>
2022-06-20 21:59:00 +0700

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,8 @@
+#Resource to create s3 bucket 
+resource "aws_s3_bucket" "terraform_state_bucket" { 
+  bucket = "quangtv-s3-11" 
+  tags = { 
+    Name = "S3Bucket" 
+    Environment = "dev" 
+  } 
+}
