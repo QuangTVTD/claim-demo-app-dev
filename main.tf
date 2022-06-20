0680723edb0e897de3a3b6e815d83c553099aa1d
Quang Tong <101450978+QuangTVTD@users.noreply.github.com>
2022-06-20 09:49:53 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "claim_dev_server" {
-ami = "ami-02354e95b39ca8dec"
+ami = "ami-943034724150"
 instance_type = "t2.micro"
 tags = {
 Name = "claim_dev_server_1"
